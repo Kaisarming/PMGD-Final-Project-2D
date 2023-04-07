ccdb0b36f2c0d64559eb68d0dbdd2f35396dc5d1
TasrikaSalma <tasrika.salma@student.pradita.ac.id>
2023-04-07 20:11:46 +0700

    ,

--- PMGD_Final_Project_2D/Assets/Scripts/RestartGameBtn.cs
+++ PMGD_Final_Project_2D/Assets/Scripts/RestartGameBtn.cs
@@ -8,10 +8,7 @@ public class RestartGameBtn : MonoBehaviour
     public GameObject car;
     private Vector3 carInitialPosition;
 
-    void Start()
-    {
-        carInitialPosition = car.transform.position; // Store the initial position of the car
-    }
+   
     // Start is called before the first frame update
     public void Restart()
     {
