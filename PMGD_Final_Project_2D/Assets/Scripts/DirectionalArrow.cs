fbc9625f968a5f5af767d71f0d9001022caf50e8
hilmihakim18 <hilmihakim18@gmail.com>
2023-04-11 13:41:15 +0700

    Game2D update

---
+++ PMGD_Final_Project_2D/Assets/Scripts/DirectionalArrow.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionalArrow : MonoBehaviour
+{
+    public Transform target;
+
+    //private void Update()
+    //{
+    //    Vector3 targetPosition = target.transform.position;
+    //    targetPosition.y = transform.position.y;
+    //    transform.LookAt(targetPosition);
+    //}
+
+    void Update()
+    {
+        lookAt();
+    }
+
+    private void lookAt()
+    {
+        transform.up = target.transform.position - transform.position;
+    }
+}
