b093ff46eed37f3184b36c2db93b578c219b900e
hilmihakim18 <hilmihakim18@gmail.com>
2023-04-11 10:30:14 +0700

    Update3D

--- PMGD_Final_Project_3D/Assets/Scripts/Player/PlayerMovement.cs
+++ PMGD_Final_Project_3D/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,11 +4,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Animator playerAnim;
+	CharacterController character;
+	public Animator playerAnim;
 	public Rigidbody playerRigid;
-    public float w_speed, wb_speed, olw_speed, rn_speed, ro_speed;
+    public float w_speed, wb_speed, olw_speed, rn_speed, ro_speed, bd_speed;
     public bool walking;
     public Transform playerTrans;
+	public GameObject cam;
+	float rotX, rotY;
+	public float sensitivity = 30.0f;
+	public bool webGLRightClickRotation = true;
+	public bool mbidik;
+	public GameObject pistol;
+
+	private void Start()
+    {
+		//pistol.SetActive(false);
+		character = GetComponent<CharacterController>();
+		if (Application.isEditor)
+		{
+			webGLRightClickRotation = false;
+			sensitivity = sensitivity * 1.5f;
+		}
+	}
 
     private void FixedUpdate()
     {
@@ -24,6 +42,20 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+		rotX = Input.GetAxis("Mouse X") * sensitivity;
+		rotY = Input.GetAxis("Mouse Y") * sensitivity;
+
+		if (webGLRightClickRotation)
+		{
+			if (Input.GetKey(KeyCode.Mouse0))
+			{
+				CameraRotation(cam, rotX, rotY);
+			}
+		}
+		else if (!webGLRightClickRotation)
+		{
+			CameraRotation(cam, rotX, rotY);
+		}
 		if (Input.GetKeyDown(KeyCode.W))
 		{
 			playerAnim.SetTrigger("walk");
@@ -69,5 +101,84 @@ public class PlayerMovement : MonoBehaviour
 				playerAnim.SetTrigger("walk");
 			}
 		}
+		if (pistol == true)
+        {
+			if (Input.GetKeyDown(KeyCode.Mouse1))
+			{
+				playerAnim.SetTrigger("pistolidle");
+				playerAnim.ResetTrigger("idle");
+				mbidik = true;
+			}
+			if (Input.GetKeyUp(KeyCode.Mouse1))
+			{
+				playerAnim.ResetTrigger("pistolidle");
+				playerAnim.SetTrigger("idle");
+				mbidik = false;
+			}
+			if (mbidik == true)
+			{
+				if (Input.GetKeyDown(KeyCode.LeftControl))
+				{
+					playerAnim.SetTrigger("pistolcrouch");
+					playerAnim.ResetTrigger("pistolidle");
+				}
+				if (Input.GetKeyUp(KeyCode.LeftControl))
+				{
+					playerAnim.ResetTrigger("pistolcrouch");
+					playerAnim.SetTrigger("pistolidle");
+				}
+				if (Input.GetKeyDown(KeyCode.W))
+				{
+					w_speed = w_speed + bd_speed;
+					playerAnim.SetTrigger("pistolwalk");
+					playerAnim.ResetTrigger("pistolidle");
+				}
+				if (Input.GetKeyUp(KeyCode.W))
+				{
+					w_speed = olw_speed;
+					playerAnim.ResetTrigger("pistolwalk");
+					playerAnim.SetTrigger("pistolidle");
+				}
+				if (Input.GetKeyDown(KeyCode.S))
+				{
+					w_speed = w_speed + bd_speed;
+					playerAnim.SetTrigger("pistolwalkback");
+					playerAnim.ResetTrigger("pistolidle");
+				}
+				if (Input.GetKeyUp(KeyCode.S))
+				{
+					w_speed = olw_speed;
+					playerAnim.ResetTrigger("pistolwalkback");
+					playerAnim.SetTrigger("pistolidle");
+				}
+				//if (Input.GetKeyDown(KeyCode.A))
+				//{
+				//	playerAnim.SetTrigger("pistolleft");
+				//	playerAnim.ResetTrigger("pistolidle");
+				//	//playerRigid.velocity = -transform.forward * w_speed * Time.deltaTime;
+				//}
+				//if (Input.GetKeyUp(KeyCode.A))
+				//{
+				//	playerAnim.ResetTrigger("pistolleft");
+				//	playerAnim.SetTrigger("pistolidle");
+				//}
+				//if (Input.GetKeyDown(KeyCode.D))
+				//{
+				//	playerAnim.SetTrigger("pistolright");
+				//	playerAnim.ResetTrigger("pistolidle");
+				//	//playerRigid.velocity = transform.forward * w_speed * Time.deltaTime;
+				//}
+				//if (Input.GetKeyUp(KeyCode.D))
+				//{
+				//	playerAnim.ResetTrigger("pistolright");
+				//	playerAnim.SetTrigger("pistolidle");
+				//}
+			}
+		}
+	}
+	void CameraRotation(GameObject cam, float rotX, float rotY)
+	{
+		transform.Rotate(0, rotX * Time.deltaTime, 0);
+		cam.transform.Rotate(-rotY * Time.deltaTime, 0, 0);
 	}
 }
