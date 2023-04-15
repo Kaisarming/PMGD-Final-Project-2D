ed38ef9cd7937788c13891bb09d2d1cb4ceb228d
Matthew George Wellson <102170142+Kaisarming@users.noreply.github.com>
2023-04-15 21:30:46 +0700

    .

--- PMGD_Final_Project_2D/Assets/Scripts/AI Scripts/CarAI.cs
+++ PMGD_Final_Project_2D/Assets/Scripts/AI Scripts/CarAI.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class CarAI : MonoBehaviour
 {
-
-    public float speed = 5f;
+public float speed = 5f;
     public Transform[] waypoints;
     private int currentWaypointIndex = 0;
+    public float rotationSpeed = 5f;
+    public float waypointRadius = 1f;
+    private Vector2 direction;
 
     private void Update()
     {
         MoveToWaypoint();
+        direction = (waypoints[currentWaypointIndex].position - transform.position).normalized;
     }
 
     private void MoveToWaypoint()
@@ -20,6 +23,8 @@ public class CarAI : MonoBehaviour
         {
             Transform targetWaypoint = waypoints[currentWaypointIndex];
             transform.position = Vector2.MoveTowards(transform.position, targetWaypoint.position, speed * Time.deltaTime);
+            float angle = Vector2.SignedAngle(transform.right, direction);
+            transform.Rotate(0f, 0f, angle * rotationSpeed * Time.deltaTime);
             if (transform.position == targetWaypoint.position)
             {
                 currentWaypointIndex++;
@@ -29,5 +34,5 @@ public class CarAI : MonoBehaviour
         {
             currentWaypointIndex = 0;
         }
-    }
-}
+    } 
+} 
