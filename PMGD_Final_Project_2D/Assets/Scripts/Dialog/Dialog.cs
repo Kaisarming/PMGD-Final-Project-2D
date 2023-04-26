25ca3a00e87d8aaba9b79087eb3121902dc14b34
TasrikaSalma <tasrika.salma@student.pradita.ac.id>
2023-04-27 03:54:54 +0700

    dialog update cutscene
    Besok dibenerin masih bug

--- PMGD_Final_Project_2D/Assets/Scripts/Dialog/Dialog.cs
+++ PMGD_Final_Project_2D/Assets/Scripts/Dialog/Dialog.cs
@@ -1,18 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Dialog : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject dialogueBox;
+    public Text dialogueText;
+    public Image characterSprite;
+    public Sprite[] characterSprites;
+    public string[] dialogueLines;
+    public int currentLine;
+    public Button continueButton;
+
+    private bool isTyping;
+    private string currentText;
+    private Coroutine typingCoroutine;
+    public float typingSpeed = 0.05f;
+
+
     void Start()
     {
-        
+        continueButton.onClick.AddListener(ContinueDialogue);
+       
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (currentLine >= dialogueLines.Length)
+        {
+            return;
+        }
+
+        if (isTyping)
+        {
+            dialogueText.text = currentText;
+        }
+        else
+        {
+            currentText = dialogueLines[currentLine];
+            typingCoroutine = StartCoroutine(TypingEffect(currentText));
+            characterSprite.sprite = characterSprites[currentLine];
+            currentLine++;
+        }
+    }
+
+    void ContinueDialogue()
+    {
+        if (isTyping)
+        {
+            StopCoroutine(typingCoroutine);
+            dialogueText.text = currentText;
+            isTyping = false;
+        }
+        else
+        {
+            isTyping = true;
+        }
+    }
+
+    IEnumerator TypingEffect(string text)
+    {
+        dialogueText.text = "";
+        isTyping = true;
+        foreach (char letter in text.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(0.05f);
+        }
+        isTyping = false;
     }
 }
+
